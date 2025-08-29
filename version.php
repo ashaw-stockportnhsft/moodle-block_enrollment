68619d3c27acc625e0a8d89976035049f1d048f6
ashaw-stockportnhsft <139889893+ashaw-stockportnhsft@users.noreply.github.com>
2025-08-29 10:57:07 +0100

    Update version.php

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = '2.0.1';
+$plugin->release = '2.0.2';
 
-$plugin->version   = 2015050101;
+$plugin->version   = 2025080101;
 $plugin->requires  = 2014111000; //What is minimum version of Moodle required: 2014111000=v2.8
 $plugin->component = 'block_enrollment';
